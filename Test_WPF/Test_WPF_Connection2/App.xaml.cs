12ae85cf5b35180e841a72eadc2e149a395968b4
Janguy <site.a.janguy@gmail.com>
2013-06-10 14:02:20 +0200

    Test WPF

---
+++ Test_WPF/Test_WPF_Connection2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Test_WPF_Connection2
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
