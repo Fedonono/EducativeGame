e09337eae12bbbed8195219dac496933a6b26e9c
Janguy <site.a.janguy@gmail.com>
2013-06-12 16:45:42 +0200

    Début de boxinfouser

---
+++ Test_WPF/Test_WPF/BoxUserInfo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_WPF
+{
+    /// <summary>
+    /// Logique d'interaction pour BoxUserInfo.xaml
+    /// </summary>
+    public partial class BoxUserInfo : UserControl
+    {
+        public BoxUserInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void image1_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+    }
+}
