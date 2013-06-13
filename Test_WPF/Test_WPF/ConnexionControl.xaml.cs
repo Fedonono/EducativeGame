cd1020eeef1557f20bf26a32c4c50b91e3b726b5
Fedonono <arnaud.babol@gmail.com>
2013-06-14 01:35:23 +0200

    Inscription finie, new db

--- Test_WPF/Test_WPF/ConnexionControl.xaml.cs
+++ Test_WPF/Test_WPF/ConnexionControl.xaml.cs
@@ -26,31 +26,80 @@ namespace Test_WPF
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            for (int i = 2012; i > 1930; i--)
+            List<Datas.Grade> grades = Bdd._db.Grades.ToList();
+            foreach (Datas.Grade grade in grades)
             {
-                this.comboBoxBirth.Items.Add(i);
+                this.comboBoxInscrForm.Items.Add(grade);
             }
-            this.comboBoxBirth.SelectedItem = this.comboBoxBirth.Items.GetItemAt(0);
+            if (grades.Count > 0)
+            {
+                this.comboBoxInscrForm.SelectedItem = this.comboBoxInscrForm.Items.GetItemAt(0);
+            }
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            App.mainWindow.connexionControlClicked(sender,e ,true);
+            this.login(sender, e);
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            App.mainWindow.connexionControlClicked(sender, e, false);
-            this.registerUser();
+            if (this.RegisterUser())
+            {
+                App.mainWindow.connexionControlClicked(sender, e, false);
+            }
+            else
+            {
+            }
         }
 
-        private void registerUser()
+        private void login(object sender, RoutedEventArgs e)
         {
-            string passhash = Bdd.SHA1(this.passwordBoxInscr.Password);
-            Datas.User u = new Datas.User() { username = this.textBoxInscrPseudo.Text, password = passhash, mail = this.textBoxInscrMail.Text, firstName = this.textBoxInscrPrenom.Text, name = this.textBoxInscrNom.Text, idGrade = 2, idRank = 1 };
-            
-            Bdd._db.AddToUsers(u);
-            Bdd._db.SaveChanges();
+            string shaPass = Bdd.SHA1(this.passwordBoxConn.Password);
+            Datas.User user = (from u in Bdd._db.Users where u.username == this.textBoxConn.Text && u.password == shaPass select u).FirstOrDefault();
+            if (user != null)
+            {
+                App.User = user;
+                App.mainWindow.connexionControlClicked(sender,e ,true);
+            }
+        }
+
+        private bool RegisterUser()
+        {
+            // todo gerer exception pour savoir si pas de doublon au niveau de l'username
+            string uName = this.textBoxInscrPseudo.Text;
+            DateTime uDate = this.dateBirth.DisplayDate;
+            string uPass = this.passwordBoxInscr.Password;
+            bool check = true;
+
+            if (uName == "")
+            {
+                this.lUsername.Content = "Veuillez saisir un nom d'utilisateur";
+                check = false;
+            }
+            if (uPass == "")
+            {
+                this.lPass.Content = "Veuillez saisir un mot de passe";
+                check = false;
+            }
+            if (!check)
+            {
+                return false;
+            }
+            string passhash = Bdd.SHA1(uPass);
+            try
+            {
+                Datas.Grade grade = (Datas.Grade)this.comboBoxInscrForm.SelectedItem;
+                Datas.User u = new Datas.User() { username = this.textBoxInscrPseudo.Text, password = passhash, mail = this.textBoxInscrMail.Text, firstName = this.textBoxInscrPrenom.Text, name = this.textBoxInscrNom.Text, birthDate = uDate, idRank = 1, idGrade = grade.ID };
+                Bdd._db.AddToUsers(u);
+                Bdd._db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                this.lError.Content = "Erreur lors de l'ajout de l'inscription dans la BDD, veuillez réesayer ou contacter un administrateur.";
+                return false;
+            }
         }
     }
 }
