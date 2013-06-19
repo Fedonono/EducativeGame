ef33059d5d19122ebda60d95d9050514267d5216
Fedonono <arnaud.babol@gmail.com>
2013-06-19 16:54:31 +0200

    Ajout Data.cs, pour surchargé/modif les classes partielles à l'extérieur du fichier généré automatiquement.

---
+++ Test_WPF/Test_WPF/Datas/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_WPF.Datas
+{
+    public partial class Grade
+    {
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
